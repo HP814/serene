f39d062ff8ca04b2aa5df465a866d43572d1296f
HP814 <98508298+HP814@users.noreply.github.com>
2023-03-17 21:45:59 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,73 @@
+  // Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+import { getAuth,
+    createUserWithEmailAndPassword,} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+    apiKey: "AIzaSyB8pdAb6SGtogsY0gCmrMHm2NXq0gz91oE",
+
+    authDomain: "serene-2bf35.firebaseapp.com",
+
+    projectId: "serene-2bf35",
+
+    storageBucket: "serene-2bf35.appspot.com",
+
+    messagingSenderId: "221214900181",
+
+    appId: "1:221214900181:web:b00beda8eb83732b55c75c"
+
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth()
+
+
+var fullName = document.getElementById("fullname");
+var contact = document.getElementById("contact");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var copassword = document.getElementById("copassword")
+window.signup = function (e) {
+if(password)
+
+    if(fullName.value == "" || contact.value=="" || email.value =="" || password.value ==""){
+        alert("All Field Are Required")
+    }
+    if(password.value == copassword.value){
+     
+    }
+    else{
+        alert("Password Confirmation is Wrong")
+        return false
+    }
+
+    e.preventDefault();
+    var obj = {
+      firstName: fullName.value,
+      contact: contact.value,
+      email: email.value,
+      password: password.value,
+    };
+  
+    createUserWithEmailAndPassword(auth, obj.email, obj.password)
+    .then(function(success){
+        window.location.replace('login.html')
+      // console.log(success.user.uid)
+      alert("signup successfully")
+    })
+    .catch(function(err){
+      alert("Error in " + err)
+    });
+   console.log()
+    console.log(obj);
+  };
